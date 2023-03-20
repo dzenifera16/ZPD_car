ece601eb28650e89d31d0a9c0ef7119d03c8f673
dzenifera16 <72212089+dzenifera16@users.noreply.github.com>
2023-03-20 14:35:46 +0200

    Add files via upload

--- engin.py
+++ engin.py
@@ -3,8 +3,8 @@ import os
 import time
 import serial
 
-
-
+bluetooth_serial = serial.Serial("/dev/ttuS0", baudrate= 9600)
+bluetooth_serial.flushInput()
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -119,140 +119,29 @@ def clean():
 #     
 # pump()
 while True:
-    command = input("Enter a command: ")
+    
+    if bluetooth_serial.in_waiting > 0:
+        command = bluetooth_serial.readline().decode().strip()
     velocity = 0
-    if command == 'f':
+    if command == 'F':
         velocity = 100
         move_forward(velocity)
-    elif command == 'b':
+    elif command == 'B':
         velocity = 100
         move_backward(velocity)
-    elif command == 'l':
+    elif command == 'L':
         velocity = 100
         turn_left(velocity)
-    elif command == 'r':
+    elif command == 'R':
         velocity = 100
         turn_right(velocity)
-    elif command == 's':
+    elif command == 'S':
         stop()
     
     else:
         print("Invalid command")
     time.sleep(0.1)
 
-# 
-# import RPi.GPIO as GPIO
-# import time
-# 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-# # Set the GPIO pins for the left motor
-# in1 = 15
-# in4 = 22
-# pin2 = 5
-# 
-# 
-# 
-# # Set the GPIO pins for the right motor
-# in2 = 16
-# in3 = 18
-# pin1 = 3
-# 
-# 
-# # Set up the GPIO pins
-# 
-# GPIO.setup(pin1,GPIO.OUT)
-# GPIO.setup(pin2,GPIO.OUT)
-# 
-# GPIO.setup(in1,GPIO.OUT)
-# GPIO.setup(in2,GPIO.OUT)
-# GPIO.setup(in4,GPIO.OUT)
-# GPIO.setup(in3,GPIO.OUT)
-# 
-# # # Set the PWM for the motor enable pins
-# M1_PWM = GPIO.PWM(pin1, 100)
-# M2_PWM = GPIO.PWM(pin2, 100)
-# M1_PWM.start(0)
-# M2_PWM.start(0)
-# 
-# def move_forward():
-#     GPIO.setup(pin1,GPIO.OUT)
-#     GPIO.setup(pin2,GPIO.OUT)
-#     
-#     GPIO.setup(in1,GPIO.OUT)
-#     GPIO.setup(in2,GPIO.OUT)
-#     GPIO.setup(in4,GPIO.OUT)
-#     GPIO.setup(in3,GPIO.OUT)
-#     
-#     GPIO.output(in1,True)#forwards left wheel
-#     GPIO.output(in2,False)# backwards right wheel
-#     GPIO.output(in3,True)#forwards right wheel
-#     GPIO.output(in4,False)# backwards left wheel
-#     time.sleep(0.5)
-#   
-#     M1_PWM.ChangeDutyCycle(velocity)
-#     M2_PWM.ChangeDutyCycle(velocity)
-# 
-# def move_backward(velocity):
-#     GPIO.setup(pin1,GPIO.OUT)
-#     GPIO.setup(pin2,GPIO.OUT)
-#     GPIO.output(in1,True)#forwards left wheel
-#     GPIO.output(in2,False)# backwards right wheel
-#     GPIO.output(in3,True)#forwards right wheel
-#     GPIO.output(in4,False)# backwards left wheel
-#     time.sleep(0.5)
-#     M1_PWM.ChangeDutyCycle(velocity)
-#     M2_PWM.ChangeDutyCycle(velocity)
-# 
-# def turn_left(velocity):
-#     
-#     GPIO.output(in1,False)#forwards left wheel
-#     GPIO.output(in2,False)# backwards right wheel
-#     GPIO.output(in3,True)#forwards right wheel
-#     GPIO.output(in4,True)# backwards left wheel
-#     M1_PWM.ChangeDutyCycle(velocity)
-#     M2_PWM.ChangeDutyCycle(velocity)
-# 
-# def turn_right(velocity):
-#     GPIO.output(in1,True)#forwards left wheel
-#     GPIO.output(in2,True)# backwards right wheel
-#     GPIO.output(in3,False)#forwards right wheel
-#     GPIO.output(in4,False)# backwards left wheel
-#     M1_PWM.ChangeDutyCycle(velocity)
-#     M2_PWM.ChangeDutyCycle(velocity)
-# 
-# def stop():
-#     GPIO.output(in1,False)#forwards left wheel
-#     GPIO.output(in2,False)# backwards right wheel
-#     GPIO.output(in3,False)#forwards right wheel
-#     GPIO.output(in4,False)# backwards left wheel
-#     M1_PWM.ChangeDutyCycle(0)
-#     M2_PWM.ChangeDutyCycle(0)
-# def clean():
-#     GPIO.cleanup()
-# while True:
-#     command = input("Enter a command: ")
-#     velocity = 0
-#     if command == 'f':
-#         velocity = 100
-#         move_forward(velocity)
-#     elif command == 'b':
-#         velocity = 50
-#         move_backward(velocity)
-#     elif command == 'l':
-#         velocity = 50
-#         turn_left(velocity)
-#     elif command == 'r':
-#         velocity = 50
-#         turn_right(velocity)
-#     elif command == 's':
-#         stop()
-#     
-#     else:
-#         print("Invalid command")
-#     time.sleep(0.1)
-
-            
 
 
 
